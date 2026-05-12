43820e23e13f1dc01aef3d370c00255405c23575
Nashath Sara <nashathsara05@gmail.com>
2026-05-12 12:44:48 +0530

    Update AppRoutes.tsx

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -15,14 +15,10 @@ import BrowseJobs from './pages/BrowseJobs/BrowseJobs';
 import SignIn from './pages/Home/SignIn';
 import Signup from './pages/Admin/Signup';
 import Settings from './pages/Admin/Settings';
-<<<<<<< HEAD
 import TicketSuccess from './pages/TicketSuccess/TicketSuccess';
-=======
 import AdminDashboardPage from "./pages/Admin/AdminDashboardPage";
 import CandidateDetails from "./pages/CandidateDetails/CandidateDetails";
 import ProfileCreate from "./pages/ProfileCreate/ProfileCreate";
-
->>>>>>> f4e0c30c67eb3802daa50f825e20b6c3c9c78743
 
 const AppRoutes = () => {
   return (
